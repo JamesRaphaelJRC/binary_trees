b6a74ec54a17fc65640ad365b20845f51f862ed7
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-08-03 06:54:50 -0700

    Calc tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t l_ht = 0, r_ht = 0;
+	size_t l_height, r_height;
 
-		l_ht = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r_ht = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((l_ht > r_ht) ? l_ht : r_ht);
-	}
-	return (0);
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		l_height = 1 + binary_tree_height(tree->left);
+	else
+		l_height = 0;
+	if (tree->right)
+		r_height = 1 + binary_tree_height(tree->right);
+	else
+		r_height = 0;
+
+	return ((l_height > r_height) ? l_height : r_height);
 }
