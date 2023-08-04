a8032ebead85dd99d1ace849412363b8425d9cbf
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-08-04 01:55:33 -0700

    Check if BT is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,19 +9,17 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int condition;
-
 	if (tree == NULL)
 		return (0);
 
-	if ((tree->left != NULL && tree->right == NULL)
-		|| (tree->left == NULL && tree->right != NULL))
-			return (1);
-	else
-		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	condition = binary_tree_is_full(tree->left);
-	condition = binary_tree_is_full(tree->right);
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
+	}
 
-	return (condition);
+	return (0);
 }
